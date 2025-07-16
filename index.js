6cb3acb033c705f8867125d5ba896a181b7da846
SamairaGarg <gargsamaira4@gmail.com>
2025-07-16 12:23:28 +0530

    Added project files

---
+++ index.js
@@ -0,0 +1,11 @@
+const pack=require ("samg14");
+console.log(pack);
+
+let sum = pack.sum(2,4)
+console.log(sum);
+
+let multiple = pack.multiple(2,4)
+console.log(multiple);
+
+let divide = pack.divide(2,4)
+console.log(divide);
